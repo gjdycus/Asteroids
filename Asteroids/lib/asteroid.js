c5a026b7a9d78fd5e216326ae9f8bb4ac5aae1ce
Garrett Dycus <gjdycus@gmail.com>
2015-10-28 13:32:40 -0700

    Refactored code

--- Asteroids/lib/asteroid.js
+++ Asteroids/lib/asteroid.js
@@ -6,13 +6,34 @@ var Asteroids;
     Asteroids = root.Asteroids = {};
   }
 
-  var Asteroid = Asteroids.Asteroid = function (pos, game) {
-    Asteroids.MovingObject.call(this, {pos: pos, color: Asteroids.Asteroid.COLOR, radius: Asteroids.Asteroid.RADIUS, vel: Asteroids.Util.randomVec(2), game: game});
+  var Asteroid = Asteroids.Asteroid = function (options) {
+    options.color = Asteroid.COLOR;
+    options.pos = options.pos || options.game.randomPosition();
+    options.radius = Asteroid.RADIUS;
+    options.vel = options.vel || Asteroids.Util.randomVec(Asteroid.SPEED);
+    Asteroids.MovingObject.call(this, options);
   };
 
   Asteroid.RADIUS = 50;
   Asteroid.COLOR = "#9D9D92";
+  Asteroid.SPEED = 3;
 
   Asteroids.Util.inherits(Asteroid, Asteroids.MovingObject);
 
+  Asteroid.prototype.collideWith = function (otherObject) {
+    if (otherObject instanceof Asteroids.Ship) {
+      otherObject.relocate();
+    }
+  };
+
+  Asteroid.prototype.draw = function (context) {
+    var asteroid = new Image(Asteroid.RADIUS*2, Asteroid.RADIUS*2);
+    var radians = this.vel[0] < 0 ? Math.PI - (Math.atan(-this.vel[1]/this.vel[0])) : Math.atan(this.vel[1]/this.vel[0]);
+    asteroid.src = "./lib/asteroid.png";
+    context.save();
+    context.translate(this.pos[0], this.pos[1]);
+    context.rotate(radians);
+    context.drawImage(asteroid, -this.radius, -this.radius, this.radius*2, this.radius*2);
+    context.restore();
+  };
 })(this);
